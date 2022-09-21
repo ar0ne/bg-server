49c36b7f8094a27ac64fa4fee09d5a490f0a5396
ar0ne <camtbitakou@yandex.ru>
2022-09-22 00:56:43 +0800

    added logic for playing cards

--- server/app/games/regicide/game.py
+++ server/app/games/regicide/game.py
@@ -3,7 +3,7 @@ import enum
 import random
 from dataclasses import dataclass
 from itertools import product
-from typing import List, Optional, Iterable
+from typing import List, Optional, Iterable, Union, Tuple
 
 HEARTS = "hearts"
 SPADES = "spades"
@@ -11,13 +11,25 @@ CLUBS = "clubs"
 DIAMONDS = "diamonds"
 SUITS = (CLUBS, DIAMONDS, HEARTS, SPADES)
 
+ACE = "A"
 KING = "K"
 QUEEN = "Q"
-ACE = "A"
 JACK = "J"
 
 # FIXME: should be dynamic
 HAND_SIZE = 7
+
+HEALTH = {
+    JACK: 20,
+    QUEEN: 30,
+    KING: 40,
+}
+
+ATTACK = {
+    JACK: 10,
+    QUEEN: 15,
+    KING: 20,
+}
 
 
 @dataclass(frozen=True)
@@ -45,12 +57,22 @@ class Rank:
         return DIAMONDS
 
 
-@dataclass
+@dataclass(frozen=True)
 class Card:
     """Card"""
 
     suit: str
     rank: Rank
+
+    @property
+    def health(self) -> Optional[int]:
+        if self.suit in HEALTH:
+            return HEALTH[self.suit]
+
+    @property
+    def attack(self) -> Optional[int]:
+        if self.suit in ATTACK:
+            return ATTACK[self.suit]
 
 
 class Deck:
@@ -63,7 +85,14 @@ class Deck:
         self.cards = cards
 
     def peek(self) -> Optional[Card]:
+        """Peek first element from the deck"""
         return self.cards[0] if self.cards else None
+
+    def pop(self) -> Card:
+        """Pop first element from the deck"""
+        card = self.cards[0]
+        self.cards = self.cards[1:]
+        return card
 
     def draw_cards(self, count: int = 1) -> List[Card]:
         """Draw cards and reveal them from deck"""
@@ -73,6 +102,19 @@ class Deck:
         draw = self.cards[:count]
         self.cards = self.cards[count:]
         return draw
+
+    def append(self, cards: Union[Card, List[Card]]) -> None:
+        """Append single or several cards to the end of a deck"""
+        if isinstance(cards, List):
+            self.cards = self.cards + cards
+        else:
+            self.cards.append(cards)
+
+    def clear(self) -> int:
+        """Return size of cleaned deck"""
+        size = len(self.cards)
+        self.cards = []
+        return size
 
     def __str__(self) -> str:
         """To string"""
@@ -84,6 +126,7 @@ class Deck:
 
 
 def cycle(iters: Iterable):
+    """Infinite loop generator"""
     while True:
         for it in iters:
             yield it
@@ -111,6 +154,9 @@ class Game:
         self.first_player = self._next_player_turn()
         self.state = GameState.created
         self.players_hand = {}
+        # represents cards played against current enemy before they went to discard pile
+        self.played_cards_deck = Deck()
+        self.turn = 0
 
     def start_game(self) -> None:
         """Player draw cards"""
@@ -121,6 +167,9 @@ class Game:
 
     def _next_player_turn(self) -> Player:
         """Change first player to next"""
+        if len(self.players) == 1:
+            # no need to update first player if it's solo game
+            return self.first_player
         next_player = next(self.next_player_loop)
         self.first_player = next_player
         return next_player
@@ -137,25 +186,70 @@ class Game:
             raise Exception  # FIXME
         if not cards:
             raise Exception  # FIXME
+        if player != self.first_player:
+            raise Exception  # FIXME
         for card in cards:
-            if card not in self.players_hand[player]:
+            if card not in self.players_hand[player.id]:
                 raise Exception  # FIXME
+        # TODO: check if it's combination of Ace and any card
+        # TODO: check if it's 2+2+.., 3+3+.. combo
 
-    def play_cards(self, player: Player, cards: List[Card]):
+    def is_current_enemy_defeated(self) -> bool:
+        """True if enemy defeated, True if sum of played cards is equal to enemy health"""
+        cards = self.played_cards_deck.cards
+        subtracted_health = 2  # FIXME: calculate all cards and their suits power
+        enemy = self.enemy_deck.peek()
+        if enemy.health <= subtracted_health:
+            return True
+        return False
+
+    def _defeat_enemy(self) -> None:
+        """Defeat current enemy"""
+        enemy = self.enemy_deck.pop()
+        # TODO: move to defeated enemies
+        if not len(self.enemy_deck):
+            # transit to won state if enemy deck is empty now
+            self.state = GameState.won
+        # remove all played cards from the deck
+        self.played_cards_deck.clear()
+
+    def play_cards(self, player: Player, cards: Union[Card, List[Card]]):
         """Play cards"""
+        if isinstance(cards, Card):
+            cards = [cards]
         self.assert_can_play_cards(player, cards)
+
+        self.turn += 1
+        # reveal cards from player's hand
+        self.players_hand[player.id] = list(
+            filter(lambda c: c not in cards, self.players_hand[player.id])
+        )
+        # add cards to played cards deck
+        self.played_cards_deck.append(cards)
+        # check has been enemy defeated
+        if self.is_current_enemy_defeated():
+            self._defeat_enemy()
+
+        self._next_player_turn()
 
     def get_game_state(self) -> dict:
         """Returns state of the game and all public information"""
+        enemy = self.enemy_deck.peek()
         return {
             "discard_deck_size": len(self.discard_deck),
-            "enemy": self.enemy_deck.peek(),
+            "played_deck": str(self.played_cards_deck),
+            "enemy": {
+                "health": enemy.health,
+                "attack": enemy.attack,
+            },
             "first_player": self.first_player,
             "players": self.players,
             "state": self.state,
+            "turn": self.turn,
             # players hands (perhaps depend on current user)
+            # TODO: add hand size
             "hands": {
-                player: self.players_hand[player.id]
+                player: str(self.players_hand[player.id])
                 for player in self.players
                 if player.id in self.players_hand
             },
@@ -180,11 +274,15 @@ class Game:
 
 
 if __name__ == "__main__":
-    pl = Player("123")
-    pl2 = Player("jee")
-    game = Game([pl, pl2])
+    pl1 = Player("Joe")
+    pl2 = Player("Bob")
+    game = Game([pl1, pl2])
 
     print(game.get_game_state())
     game.start_game()
+    print(game.get_game_state())
+    game.play_cards(pl1, [game.players_hand[pl1.id][0]])
+    print(game.get_game_state())
+    game.play_cards(pl2, [game.players_hand[pl2.id][4]])
     print(game.get_game_state())
 
