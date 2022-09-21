a379bbe36ef2905a7acf8d28526f5af65463c094
ar0ne <camtbitakou@yandex.ru>
2022-09-21 13:36:08 +0800

    small code factoring

---
+++ server/app/models.py
@@ -0,0 +1,58 @@
+"""DB models"""
+
+from datetime import datetime
+
+from pony.orm import Required, Optional, PrimaryKey, Set, Database, sql_debug, db_session
+
+db = Database()
+
+
+class Player(db.Entity):
+    admin_rooms = Set("Room", reverse="admin")
+    date_joined = Required(datetime)
+    email = Required(str, unique=True)
+    id = PrimaryKey(int, auto=True)  # FIXME: uuid4
+    joined_rooms = Set("Room", reverse="participants")
+    name = Required(str)
+    nickname = Required(str, unique=True)
+    password = Required(str)
+
+
+class Game(db.Entity):
+    id = PrimaryKey(int, auto=True)  # FIXME: uuid4
+    name = Required(str)
+    games = Set("Room")
+
+
+class Room(db.Entity):
+    admin = Required(Player)
+    date_closed = Optional(datetime)
+    date_created = Required(datetime)
+    game = Required(Game)
+    id = PrimaryKey(int, auto=True)  # FIXME: uuid4
+    participants = Set(Player)
+    status = Required(int)  # FIXME: Enum?
+
+
+sql_debug(True)
+
+def init_fake_data():
+    with db_session:
+        game = Game(name="Regicide")
+        joe = Player(
+            date_joined=datetime.now(),
+            email="joe@pl.er",
+            name="Joe",
+            nickname="joe",
+            password="123",
+        )
+        bob = Player(
+            date_joined=datetime.now(),
+            email="bob@pl.er",
+            name="Bob",
+            nickname="bob",
+            password="123",
+        )
+        room = Room(
+            admin=joe, date_created=datetime.now(), game=game, participants=[joe, bob], status=1
+        )
--- server/main.py
+++ server/main.py
@@ -3,77 +3,25 @@ Main
 """
 import asyncio
 import os
-from datetime import datetime
 
 import tornado.web
-from pony.orm import Database, Required, PrimaryKey, Set, sql_debug, db_session, Optional, select
+from pony.orm import db_session, select, Database
 
-from tornado.options import define, options, parse_command_line
+from tornado.options import define, options, parse_command_line, parse_config_file
+
+from server.app import models
+from server.app.models import Room
 
 define("port", default=8888, help="run on the given port", type=int)
 define("debug", default=True, help="run in debug mode")
+define("db_provider", default="sqlite", help="database provider")
 define("db_host", default="127.0.0.1", help="database host")
 define("db_port", default=5432, help="database port")
-define("db_database", default="blog", help="database name")
-define("db_user", default="blog", help="database user")
-define("db_password", default="blog", help="database password")
+define("db_database", default="", help="database name")
+define("db_user", default="", help="database user")
+define("db_password", default="", help="database password")
 
-db = Database("sqlite", filename=":memory:")
-
-class Player(db.Entity):
-    admin_rooms = Set("Room", reverse="admin")
-    date_joined = Required(datetime)
-    email = Required(str, unique=True)
-    id = PrimaryKey(int, auto=True)  # FIXME: uuid4
-    joined_rooms = Set("Room", reverse="participants")
-    name = Required(str)
-    nickname = Required(str, unique=True)
-    password = Required(str)
-
-
-class Game(db.Entity):
-    id = PrimaryKey(int, auto=True)  # FIXME: uuid4
-    name = Required(str)
-    games = Set("Room")
-
-
-class Room(db.Entity):
-    admin = Required(Player)
-    date_closed = Optional(datetime)
-    date_created = Required(datetime)
-    game = Required(Game)
-    id = PrimaryKey(int, auto=True)  # FIXME: uuid4
-    participants = Set(Player)
-    status = Required(int)  # FIXME: Enum?
-
-
-def create_database():
-
-    sql_debug(True)
-
-    db.generate_mapping(create_tables=True)
-
-    with db_session:
-        game = Game(name="Regicide")
-        joe = Player(
-            date_joined=datetime.now(),
-            email="joe@pl.er",
-            name="Joe",
-            nickname="joe",
-            password="123",
-        )
-        bob = Player(
-            date_joined=datetime.now(),
-            email="bob@pl.er",
-            name="Bob",
-            nickname="bob",
-            password="123",
-        )
-        room = Room(
-            admin=joe, date_created=datetime.now(), game=game, participants=[joe, bob], status=1
-        )
-
-
+CONFIG_FILE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
 
 
 class BaseRequestHandler(tornado.web.RequestHandler):
@@ -107,13 +55,31 @@ class Application(tornado.web.Application):
         super().__init__(handlers, **settings)
 
 
+def init_database() -> Database:
+    """Initialize database"""
+    db = models.db
+    db_settings = dict(
+        provider=options.db_provider,
+        user=options.db_user,
+        password=options.db_password,
+        host=options.db_host,
+        port=options.db_port,
+        database=options.db_database,
+    )
+    if options.db_provider == "sqlite":
+        db_settings = dict(provider=options.db_provider, filename=":memory:")
+
+    db.bind(**db_settings)
+    db.generate_mapping(create_tables=True)
+    return db
+
 
 async def main() -> None:
     """Main loop function"""
-
     parse_command_line()
+    parse_config_file(CONFIG_FILE_PATH)
 
-    create_database()
+    db = init_database()
     app = Application(db)
     app.listen(options.port)
     await asyncio.Event().wait()
